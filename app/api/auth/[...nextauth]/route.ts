97df0b99c7dcf6a9921ab9dfd83e6e9eb3bf8b8b
Joshua Shelley <accounts-git@straddler.club>
2025-05-23 22:26:26 -0400

    remove unused imports

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,4 @@
 import NextAuth from 'next-auth'
-import Google from 'next-auth/providers/google'
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import { PrismaClient } from '@prisma/client'
 import { authOptions } from '@/lib/auth'
 
 
